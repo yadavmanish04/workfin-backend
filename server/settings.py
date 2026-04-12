102749c43c91832157130abd5d1e34b8d421e8c0
Manishydv <manishyadav11406@gmail.com>
2026-04-12 13:26:48 +0530

    updates in email section

--- server/settings.py
+++ server/settings.py
@@ -242,8 +242,8 @@ AUTH_USER_MODEL = 'authentication.User'
 
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
