4fc707686fb34cb8d0a9af5088b53c46d5187e68
Manishydv <manishyadav11406@gmail.com>
2026-04-12 15:04:48 +0530

    updates utils.py

--- apps/authentication/utils.py
+++ apps/authentication/utils.py
@@ -1,34 +1,70 @@
+# from django.core.mail import send_mail
+# from django.conf import settings
+# from django.template.loader import render_to_string
+# from django.utils.html import strip_tags
+
+# def send_otp_email(email, otp):
+#     subject = 'WorkFina - Email Verification Code'
+    
+#     # Log OTP for debugging
+#     print(f'[OTP DEBUG] Sending OTP {otp} to {email}')
+    
+#     # HTML template render
+#     html_message = render_to_string('auth/otp_email.html', {
+#         'otp': otp,
+#         'email': email
+#     })
+    
+#     # Plain text version
+#     plain_message = strip_tags(html_message)
+    
+#     # from_email = settings.EMAIL_HOST_USER
+#     from_email = settings.DEFAULT_FROM_EMAIL
+#     recipient_list = [email]
+    
+#     send_mail(
+#         subject=subject,
+#         message=plain_message,
+#         from_email=from_email,
+#         recipient_list=recipient_list,
+#         html_message=html_message,
+#         fail_silently=False
+#     )
+    
+#     # print(f'[OTP DEBUG] OTP successfully sent to {email}')
+    
+    
+    
 from django.core.mail import send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+import logging
 
 def send_otp_email(email, otp):
     subject = 'WorkFina - Email Verification Code'
-    
-    # Log OTP for debugging
-    print(f'[OTP DEBUG] Sending OTP {otp} to {email}')
-    
-    # HTML template render
+    logging.info(f'[OTP DEBUG] Sending OTP {otp} to {email}')
+
     html_message = render_to_string('auth/otp_email.html', {
         'otp': otp,
         'email': email
     })
-    
-    # Plain text version
     plain_message = strip_tags(html_message)
-    
-    # from_email = settings.EMAIL_HOST_USER
+
     from_email = settings.DEFAULT_FROM_EMAIL
     recipient_list = [email]
+
+    try:
+        send_mail(
+            subject=subject,
+            message=plain_message,
+            from_email=from_email,
+            recipient_list=recipient_list,
+            html_message=html_message,
+            fail_silently=False
+        )
+        logging.info(f'[OTP DEBUG] OTP successfully sent to {email}')
+    except Exception as e:
+        logging.error(f'[OTP ERROR] Failed to send OTP: {e}', exc_info=True)
+        raise
     
-    send_mail(
-        subject=subject,
-        message=plain_message,
-        from_email=from_email,
-        recipient_list=recipient_list,
-        html_message=html_message,
-        fail_silently=False
-    )
-    
-    print(f'[OTP DEBUG] OTP successfully sent to {email}')
